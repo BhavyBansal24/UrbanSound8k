975e366cb1d957d3c0502b14eba2897d05bdbbca
BhavyBansal24 <bhavy.209309023@muj.manipal.edu>
2022-10-05 20:26:08 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,117 @@
+import streamlit as st
+import numpy as np
+import helper
+import librosa
+import librosa.display as idp
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+
+st.sidebar.title("UrbanSound8k Audio Classifer")
+
+Uploaded_file = st.sidebar.file_uploader("Insert File", type='.wav')
+if Uploaded_file is not None:
+    # st.write(Uploaded_file)
+    raw, sample_rate = helper.preprocess(Uploaded_file)
+
+    # Play Audio File
+    st.title("Play Uploaded File & Waveplot")
+    out = helper.play_audio(raw, sample_rate)
+    fig, ax = plt.subplots()
+    ax = idp.waveshow(raw, sr=sample_rate)
+    st.pyplot(fig)
+    st.audio(Uploaded_file, format='audio/wav')
+
+    # Printing the results
+    st.title("predicted Class")
+    prediction = helper.audio_to_result(Uploaded_file)
+    le.classes_ = np.load('classes.npy')
+    pred_class = le.inverse_transform(prediction)
+    class_ = "Predicted Class : "+ str(pred_class[0])
+    st.success(class_)
+
+    # STFT
+    st.title('spectrogram')
+    X_db = helper.Spectrogram(raw, sample_rate)
+    fig, ax = plt.subplots()
+    ax = idp.specshow(X_db, sr=sample_rate, x_axis="time", y_axis="hz")
+    plt.title("Input Audio's Spectrogram")
+    st.pyplot(fig)
+
+    # Harmonic-Percussive Separation (HPS)
+    st.title('Harmonic Percussive Separation')
+    h, p = helper.HPS(raw, sample_rate)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header("Harmonics")
+        fig, ax = plt.subplots()
+        ax = librosa.display.specshow(
+            h, y_axis='mel', x_axis='s', sr=sample_rate)
+        plt.title("Harmonic Mel Spectogram")
+        st.pyplot(fig)
+    with col2:
+        st.header("Percussion")
+        fig, ax = plt.subplots()
+        ax = librosa.display.specshow(
+            p, y_axis='mel', x_axis='s', sr=sample_rate)
+        plt.title("Percuisive Mel Spectogram")
+        st.pyplot(fig)
+
+    # MFCC
+    st.title('Mel-Frequency Cepstral Coefficients (MFCC)')
+    mfcc = helper.MFCC(raw, sample_rate)
+    fig, ax = plt.subplots()
+    ax = idp.specshow(mfcc, x_axis="s")
+    plt.title("Mel-Frequency Cepstral Coefficients")
+    st.pyplot(fig)
+
+    # Zero-Crossing
+    st.title('Zero Crossing Rate')
+    zero_crossing = helper.ZCR(raw, sample_rate)
+    fig, ax = plt.subplots()
+    ax.plot(raw[4700:5500])
+    plt.title("Zero Crossing Rate")
+    st.pyplot(fig)
+    st.write("Total Number of Zero Crossing is: ", str(sum(zero_crossing)))
+
+    # Spectral Centroid & Roll-Off
+    st.title('Spectral Centroid & Roll-Off')
+    C, R = helper.Spectral(raw, sample_rate)
+    col1, col2 = st.columns(2)
+    with col1:
+        fig, ax = plt.subplots()
+        ax = plt.semilogy(C.T, "r")
+        plt.ylabel("Hz")
+        plt.title("Spectral Centroid")
+        st.pyplot(fig)
+    with col2:
+        fig, ax = plt.subplots()
+        ax = plt.semilogy(R.T, "r")
+        plt.ylabel("Hz")
+        plt.title("Spectral Roll-Off")
+        st.pyplot(fig)
+
+    # Chroma Feature
+    st.title('Chroma Feature')
+    chroma = helper.Chroma(raw, sample_rate)
+    fig, ax = plt.subplots()
+    ax = librosa.display.specshow(chroma, y_axis='chroma', x_axis='time')
+    plt.colorbar()
+    plt.title("Chromagram")
+    st.pyplot(fig)
+
+    # RMSE & log power spectrogram
+    st.title('RMSE & log power spectrogram')
+    S, RMSEn, times = helper.RMSE(raw, sample_rate)
+    col1, col2 = st.columns(2)
+    with col1:
+        fig, ax = plt.subplots()
+        ax = plt.semilogy(times, RMSEn[0])
+        plt.title("Root Mean Squared Energy")
+        st.pyplot(fig)
+    with col2:
+        fig, ax = plt.subplots()
+        ax = librosa.display.specshow(librosa.amplitude_to_db(
+            S, ref=np.max), y_axis='log', x_axis='time')
+        plt.title("log Power Spectrogram")
+        st.pyplot(fig)
---
+++ helper.py
@@ -0,0 +1,63 @@
+
+import librosa
+import librosa.display
+import numpy as np
+import IPython.display as ipd
+import keras.models as models
+import cv2
+
+def preprocess(data):
+    audio_data, sample_rate = librosa.load(data, res_type='kaiser_fast')
+    return audio_data, sample_rate
+
+def play_audio(raw,sr):
+    out = ipd.Audio(raw,rate=sr)
+    return out
+def Spectrogram(raw,sr):
+    X=librosa.stft(raw) #stft -> Short-time Fourier transform
+    X_db=librosa.amplitude_to_db(abs(X)) #Translation from amplitude to desibel(db) value
+    return X_db
+
+def HPS(raw,sr):
+    data_h, data_p = librosa.effects.hpss(raw)
+    spec_h = librosa.feature.melspectrogram(data_h, sr=sr)
+    spec_p = librosa.feature.melspectrogram(data_p, sr=sr)
+    db_spec_h = librosa.power_to_db(spec_h,ref=np.max)
+    db_spec_p = librosa.power_to_db(spec_p,ref=np.max)
+    return db_spec_h, db_spec_p
+
+def MFCC(raw,sr):
+    mfcc=librosa.feature.mfcc(raw,sr)
+    return mfcc
+
+def ZCR(raw,sr):
+    zero_crossing=librosa.zero_crossings(raw)
+    return zero_crossing
+
+def Spectral(raw,sr):
+    spec_cent=librosa.feature.spectral_centroid(raw)
+    spec_roll=librosa.feature.spectral_rolloff(raw,sr=sr)
+    return spec_cent, spec_roll
+
+def Chroma(raw,sr):
+    chroma=librosa.feature.chroma_stft(raw, sr)
+    return chroma
+
+def RMSE(raw,sr):
+    S = librosa.magphase(librosa.stft(raw, window=np.ones, center=False))[0]
+    RMSEn= librosa.feature.rms(S=S)
+    times = librosa.times_like(RMSEn)
+    return S, RMSEn, times
+    
+def audio_to_result(filename):
+    model_saved = models.load_model('hello')
+    raw , sr = librosa.load(filename, res_type='kaiser_fast')
+    X_image = librosa.feature.mfcc(y=raw, sr=sr, n_mfcc=40)
+    up_width = 173
+    up_height = 40
+    up_points = (up_width, up_height)
+    X_image = cv2.resize(X_image, up_points, interpolation= cv2.INTER_LINEAR)
+    X_image = X_image.reshape(1,X_image.shape[0],X_image.shape[1],1)
+    pred = model_saved.predict(X_image)
+    prediction = np.argmax(pred, axis=-1)
+    return prediction
